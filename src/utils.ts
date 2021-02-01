a37d51f0b24e01424f035d1e48acacc9c02e820b
veganbeef <veganbeef@protonmail.com>
2021-02-01 15:26:38 -0600

    added comments to utils file

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,17 @@ import { join } from 'path';
 import { Message } from './schema/schema-types';
 
 
+/**
+ * Util method to read the entire database.json file and parse it into an array of Message objects
+ */
 export async function readFromJSONDatabase(): Promise<Message[]> {
   const dbString = await fsPromises.readFile(join(__dirname, 'database.json'), { encoding: 'utf-8' });
   return JSON.parse(dbString);
 }
 
+/**
+ * Util method to write a single Message object to the end of the database.json file
+ */
 export async function writeToJSONDatabase(message: Message): Promise<void> {
   const dbArray = await readFromJSONDatabase();
   dbArray.push(message);
